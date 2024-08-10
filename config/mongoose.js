03e8765d6afe748d3cd6485dafed2a8d18b374a4
Raman Sahi <sahiraman7@gmail.com>
2024-08-10 11:01:05 +0530

    changes to database

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,20 +1,18 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const DB = 'mongodb+srv://pawarsubham438:6aO4JOECgoI7Xu71@cluster0.bemtdzm.mongodb.net/mernstack?retryWrites=true&w=majority';
-
-
-
+const DB =
+  "mongodb+srv://sahiraman7:lYOEZxbYPk8IIUpo@cluster0.dncnotl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 mongoose.connect(DB, {
-	useNewUrlParser: true,
-	useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
 const db = mongoose.connection;
 
-db.on('error', console.error.bind(console, 'Error in connecting to MongoDB'));
+db.on("error", console.error.bind(console, "Error in connecting to MongoDB"));
 
-db.once('open', function () {
-	console.log('Connected to Database :: Mongodb');
+db.once("open", function () {
+  console.log("Connected to Database :: Mongodb");
 });
 
 module.exports = mongoose;
